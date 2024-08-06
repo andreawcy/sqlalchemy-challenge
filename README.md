3f3f587de09b25aa48bbb5e23aa688a75d852f17
andreawcy <andreawu1109@gmail.com>
2024-08-06 01:58:00 -0400

    Complete

---
+++ README.md
@@ -0,0 +1,19 @@
+## Module 10 Challenge
+Name: Andrea Wu
+
+### Background
+Congratulations! You've decided to treat yourself to a long holiday vacation in Honolulu, Hawaii. To help with your trip planning, you decide to do a climate analysis about the area. The following sections outline the steps that you need to take to accomplish this task.
+
+Part 1: Analyze and Explore the Climate Data
+In this section, you’ll use Python and SQLAlchemy to do a basic climate analysis and data exploration of your climate database. Specifically, you’ll use SQLAlchemy ORM queries, Pandas, and Matplotlib. 
+
+
+Part 2: Design Your Climate App
+Now that you’ve completed your initial analysis, you’ll design a Flask API based on the queries that you just developed. 
+
+### Notes to user:
+Please run "climate_starter.ipynb".
+
+For "app.py", please install Flask before you run the file.
+Please use "flask --app "insert file path" run" to run the file.
+
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,121 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify, render_template
 
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///SurfsUp/Resources/hawaii.sqlite")
 
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available API routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Query the last 12 months of precipitation data
+    latest_date_result = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    latest_date = dt.datetime.strptime(latest_date_result[0], "%Y-%m-%d").date()
+    year_ago = latest_date - dt.timedelta(days=365)
+    
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+    
+    session.close()
+
+    # Convert query results to a dictionary
+    precipitation_dict = {date: prcp for date, prcp in results}
+
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Query all stations
+    results = session.query(Station.station, Station.name).all()
+    
+    session.close()
+
+    # Convert query results to a list of dictionaries
+    stations_list = [{"station": station, "name": name} for station, name in results]
+
+    return jsonify(stations_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Query the most active station and its temperature observations for the previous year
+    latest_date_result = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    latest_date = dt.datetime.strptime(latest_date_result[0], "%Y-%m-%d").date()
+    year_ago = latest_date - dt.timedelta(days=365)
+    
+    most_active_station_result = session.query(Measurement.station, func.count(Measurement.id).label('count')).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.id).desc()).first()
+    
+    most_active_station = most_active_station_result[0]
+    
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date >= year_ago).\
+        all()
+    
+    session.close()
+
+    # Convert query results to a list of dictionaries
+    tobs_list = [{"date": date, "tobs": tobs} for date, tobs in results]
+
+    return jsonify(tobs_list)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
